f157b9f4a8e927dda0f01d6dd4b298f5170171d4
taruc <taruc@B18>
2017-11-06 12:15:32 +0800

    Hello World

---
+++ src/week4/Week4.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week4;
+
+/**
+ *
+ * @author taruc
+ */
+public class Week4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Holy SHIITT");
+    }
+    
+}
